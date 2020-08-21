a96e9326291790ab940f4adf6e8dca7225423a83
nelisiwem <pnpnelly@gmail.com>
2020-08-21 08:03:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-xx2t8g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-xx2t8g)
---
+++ index.ts
@@ -0,0 +1,32 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+function compareNumbers(number1, number2){
+  var num1 = number1;
+  var num2 = number2;
+  if (num1>num2){
+    return num2;
+  }else if
+    (num2>num1)
+    return num1
+  
+}
+
+console.log(compareNumbers(13,6));
+console.log(compareNumbers(6,13));
+
+
+
+
+
+
+
+
+
+
+
+
